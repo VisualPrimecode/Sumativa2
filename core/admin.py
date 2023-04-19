fc85b768b8b2c15cdab33b7df05e82a82a907819
VisualPrimecode <claudiorodriguez7778@gmail.com>
2023-04-18 22:52:15 -0400

    ahora si

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import Categoria, Vehiculo,Rol,Usuario,Genero
+from .models import Rol,Usuario,Genero
 
 # Register your models here.
-admin.site.register(Categoria)
-admin.site.register(Vehiculo)
 admin.site.register(Rol)
 admin.site.register(Usuario)
 admin.site.register(Genero)
