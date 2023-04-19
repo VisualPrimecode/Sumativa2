386706b3b0c2dfb0688cab563aba7247b5d6a69d
VisualPrimecode <claudiorodriguez7778@gmail.com>
2023-04-18 22:44:32 -0400

    lol

--- core/models.py
+++ core/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-
-class Categoria(models.Model):
-    idCategoria = models.IntegerField(primary_key=True, verbose_name='Id de Categoria')
-    nombreCategoria = models.CharField(max_length=50, verbose_name='Nombre de la Categoria')
-    
-    def __str__(self):
-        return self.nombreCategoria
-
-class Vehiculo(models.Model):
-    patente= models.CharField(primary_key=True, max_length=6,verbose_name='Patente')
-    marca= models.CharField(max_length=20,verbose_name='Marca vehiculo')
-    modelo= models.CharField(max_length=20, null=True , blank=True ,verbose_name='Modelo')
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.patente
-    
  
 class Rol(models.Model):
     idRol=models.IntegerField(primary_key=True, verbose_name='Id de Rol')
--- core/views.py
+++ core/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import VehiculoForm,UsuarioForm
 from .models import Vehiculo,Usuario
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+
 
 # Create your views here.
 class persona:
@@ -10,60 +14,6 @@ class persona:
         self.edad=edad
         super().__init__()
         
-def home(request):
-    
-    vehiculos = Vehiculo.objects.all()
-    datos={
-        'vehiculos':vehiculos
-    }
-    
-    hijo=persona("Juan Perez","7")
-    lista=["Accion","terror","fentinol"]
-    contexto={"nombre":"Claudio Andres", "Categorias":lista,"hijo":hijo,"Vehiculos":vehiculos}
-    return render(request,'core/home.html',contexto)
-
-def form_vehiculo(request):
-    datos={
-        'form':VehiculoForm()
-    }
-    if request.method== 'POST':
-        formulario = VehiculoForm(request.POST)
-        if formulario.is_valid:
-            formulario.save()
-            datos['mensaje'] = "Guardados correctamente"
-    return render(request, 'core/form_vehiculo.html',datos)
-
-def form_mod_vehiculo(request, id):
-    auto = Vehiculo.objects.get(patente=id)
-    datos ={
-        'form': VehiculoForm(instance=auto)
-    }
-    if request.method== 'POST':
-        formulario = VehiculoForm(data=request.POST, instance=auto)
-        if formulario.is_valid:
-            formulario.save()
-            datos['mensaje'] = "Modificado correctamente"
-      
-    return render(request, 'core/form_mod_vehiculo.html',datos)
-
-def listar_mod_vehiculo(request):
-    vehiculos = Vehiculo.objects.all()
-    datos = {
-        "vehiculos":vehiculos
-    }
-    return render(request, 'core/listar_mod_vehiculo.html',datos)
-
-def form_del_vehiculo(request, id):
-    aut = Vehiculo.objects.get(patente=id)
-    datos ={
-        'form': VehiculoForm(instance=aut)
-    }
-    if request.method== 'POST':
-        formulario = VehiculoForm(data=request.POST, instance=aut)
-        aut.delete()
-        datos['mensaje'] = "Eliminado correctamente"
-      
-    return render(request, 'core/form_del_vehiculo.html',datos)
 def MenuPrincipal(request):
     
     vehiculos = Vehiculo.objects.all()
@@ -77,7 +27,20 @@ def MenuPrincipal(request):
     return render(request,'core/menuPrincipal.html',contexto)
 
 def INICIOSESION(request):
-     return render(request,'core/INICIOSESION.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('menuPrincipal')
+        else:
+            messages.error(request, 'Credenciales inválidas')
+    usuarios = Usuario.objects.all()
+    context = {
+        'usuarios': usuarios
+    }
+    return render(request, 'core/INICIOSESION.html', context)
 def RecuperarContra(request):
      return render(request,'core/RecuperarContra.html')
 def registro(request):
@@ -87,21 +50,21 @@ from django.core.exceptions import ValidationError
 
 def form_usuario(request):
     context = {
-        'form': UsuarioForm()  # Pasar el formulario al contexto de la plantilla
+        'form': UsuarioForm()  
     }
     if request.method == 'POST':
         formulario = UsuarioForm(request.POST)
         if formulario.is_valid():
             try:
                 usuario = formulario.save(commit=False)
-                # Realizar cualquier otra operación adicional antes de guardar el modelo
+                
                 usuario.save()
                 context['mensaje'] = "Guardados correctamente"
             except ValidationError as e:
-                # Manejar la excepción de validación personalizada
-                formulario.add_error(None, e)  # Agregar el error al formulario
+                
+                formulario.add_error(None, e)  
         else:
-            # Si el formulario no es válido, mostrar los errores en el contexto
+            
             context['form'] = formulario
     return render(request, 'core/form_usuario.html', context)
 
